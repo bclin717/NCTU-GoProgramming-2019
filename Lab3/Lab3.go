eed45fed0c967f564f68d7e98dc7ab247790b736
Bo Cheng(Kevin) <ds934118@gmail.com>
2019-10-03 07:42:28 +0800

    add for loop
    add for loop

--- Lab3/Lab3.go
+++ Lab3/Lab3.go
@@ -2,12 +2,11 @@ package main
 
 import "fmt"
 
-func main() {
+func main() {	
 	var n int
-
-	fmt.Scanln(&n)
-
-	// Please complete the code to make this program compiled without error.
+	for {
+		fmt.Scanln(&n)
+	}
+	// Please complete the code to make this program be compiled without error.
 	// Notice that you can only add code in this file.
-
 }
