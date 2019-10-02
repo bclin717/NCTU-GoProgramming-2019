254da7c83d9d23982cac90313e7d2d54ddbe3b32
tsai7053 <tsai7053@gmail.com>
2019-10-02 19:51:23 +0800

    Add Lab3

---
+++ Lab3/Lab3.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+
+	fmt.Scanln(&n)
+
+	// Please complete the code to make this program compiled without error.
+	// Notice that you can only add code in this file.
+
+}
