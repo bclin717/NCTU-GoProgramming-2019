4c3c57a13d04837fd9cf28ec9a92eb4c55112cc9
Bo Cheng(Kevin) <ds934118@gmail.com>
2019-10-03 08:35:38 +0800

    Add function

--- Lab3/Lab3.go
+++ Lab3/Lab3.go
@@ -2,11 +2,16 @@ package main
 
 import "fmt"
 
-func main() {	
-	var n int
-	for {
-		fmt.Scanln(&n)
-	}
+func Sum() {
+
+}
+
+func main() {
 	// Please complete the code to make this program be compiled without error.
 	// Notice that you can only add code in this file.
+	var n int64
+	for {
+		fmt.Scanln(&n)
+
+	}
 }
