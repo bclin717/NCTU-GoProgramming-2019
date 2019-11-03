a3fb0b0c222d4504dc24bcb346e3bc428b6bbd8c
Bo Cheng(Kevin) <ds934118@gmail.com>
2019-11-03 23:04:52 +0800

    Update main.go

--- Lab5/main.go
+++ Lab5/main.go
@@ -10,23 +10,24 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	pathParts := strings.SplitN(r.URL.Path, "/", -1)
+	fmt.Fprintf(w, "hello world!")
+// 	pathParts := strings.SplitN(r.URL.Path, "/", -1)
 
-	operation := pathParts[1]
-	a, _ := strconv.Atoi(pathParts[2])
-	b, _ := strconv.Atoi(pathParts[3])
+// 	operation := pathParts[1]
+// 	a, _ := strconv.Atoi(pathParts[2])
+// 	b, _ := strconv.Atoi(pathParts[3])
 
-	if operation == "add" {
+// 	if operation == "add" {
 
-	} else if operation == "sub" {
+// 	} else if operation == "sub" {
 
-	} else if operation == "mul" {
+// 	} else if operation == "mul" {
 
-	} else if operation == "div" {
+// 	} else if operation == "div" {
 
-	} else {
-		fmt.Fprintf(w, "hello world!")
-	}
+// 	} else {
+// 		fmt.Fprintf(w, "hello world!")
+// 	}
 }
 
 func add(a, b int) int {
